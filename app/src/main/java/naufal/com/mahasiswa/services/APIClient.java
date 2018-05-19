0d529cfd28b13de32c347ff9acf9ee1ff4150241
naufalrzld <naufalrzld@gmail.com>
2018-05-19 22:43:18 +0700

    Finish

--- app/src/main/java/naufal/com/mahasiswa/services/APIClient.java
+++ app/src/main/java/naufal/com/mahasiswa/services/APIClient.java
@@ -10,7 +10,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class APIClient {
-    private static final String BASE_URL = "http://192.168.0.22/motion/";
+    private static final String BASE_URL = "http://192.168.0.22/motion/"; //Alamat IP sesuaikan dengan PC/Laptop sendiri
     private static Retrofit retrofit = null;
 
     public static Retrofit getAPIClient() {
